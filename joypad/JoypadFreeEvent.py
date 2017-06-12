145247aab26c9beaa91cf635eafd1d181da4dd76
blackbeard <blackbeard>
2017-06-12 12:19:25 +0200

    joypad

--- joypad/JoypadFreeEvent.py
+++ joypad/JoypadFreeEvent.py
@@ -42,19 +42,19 @@ for event in dev.read_loop(): # boucle qui surveille l'arrivee d'un evenement
     if e_code == 3:
         if e_value < 118 or e_value > 124:
             valPerc = (e_value / 255.)*1.
-            print "Stick 1 G<->D : ",valPerc
+            print "Stick 2 G<->D : ",valPerc
 
-    # Stick 2 H<->B
+    # Stick 1 G<->D
     if e_code == 2:
         if e_value < 118 or e_value > 124:
             valPerc = (e_value / 255.)*1.
-            print "Stick 2 H<->B : ",valPerc
+            print "Stick 1 G<->D : ",valPerc
 
     # Stick 2 G<->D
     if e_code == 4:
         if e_value < 118 or e_value > 124:
             valPerc = (e_value / 255.)*1.
-            print "Stick 2 G<->D : ",valPerc
+            print "Stick 2 H<->B : ",valPerc
 
     # L1
     if e_code == 292:
