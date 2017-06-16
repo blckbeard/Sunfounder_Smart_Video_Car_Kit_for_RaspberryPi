1851a469e148782dc03eda21467fef17013bd9b0
blackbeard <blackbeard>
2017-06-17 01:01:51 +0200

    joypad

--- joypad/JoypadFreeEvent.py
+++ joypad/JoypadFreeEvent.py
@@ -44,14 +44,13 @@ for event in dev.read_loop(): # boucle qui surveille l'arrivee d'un evenement
         if mutePrint: print "valPerc :",valPerc
         
     if e_code == 0:
-        if e_value != 0 :
-            if e_value < 127 or e_value > 129:
-                analogicStick(e_code,e_type,e_value,mutePrint=mutePrint)
+        if e_value < 128 or e_value > 128:
+            analogicStick(e_code,e_type,e_value,mutePrint=mutePrint)
     # Stick 1 H<->B Analogique
     if e_code == 1 :
         #print "Stick 1 H<->B e_value : ",e_value
         # range : 21 - 210
-        if e_value < 127 or e_value > 129:
+        if e_value < 128 or e_value > 128:
             analogicStick(e_code,e_type,e_value,mutePrint=mutePrint)
             #valPerc = (e_value / 255.)*1.
             #print "Stick 1 H<->B :",valPerc
