e61d1f656b374e17b8dcc15dba3238cf95098bb1
blackbeard <blackbeard>
2017-06-17 02:13:59 +0200

    joypad

--- joypad/JoypadFreeEvent.py
+++ joypad/JoypadFreeEvent.py
@@ -46,23 +46,7 @@ for event in dev.read_loop(): # boucle qui surveille l'arrivee d'un evenement
     exclude = [2,4]
     filterKey = [0,1,3,5]
 
-    if e_value != 0:
-        if e_code not in exclude:
-            if e_code in filterKey:
-                if e_value < 127.99 or e_value > 128.01:
-                    sendDevDict(devDict)
-            elif e_code in [17,16]:
-                sendDevDict(devDict)
-                if e_code == 17 :
-                    devDict['crossUpDown']=0
-                if e_code == 16 :
-                    devDict['crossLR']=0
-                sendDevDict(devDict)
-            else:
-                if mutePrint: print "e_code :",e_code
-                if mutePrint: print "e_type :",e_code
-                if mutePrint: print "e_value :",e_value
-                sendDevDict(devDict)
+
     # Stick 1 R<->L Analogique 
     if e_code == 0:
         if e_value != 0 :
@@ -154,5 +138,21 @@ for event in dev.read_loop(): # boucle qui surveille l'arrivee d'un evenement
     if e_code == 295:
         print "R2 :",e_value
 
-    
-
+    # send devDict
+    if e_value != 0:
+        if e_code not in exclude:
+            if e_code in filterKey:
+                if e_value < 127.99 or e_value > 128.01:
+                    sendDevDict(devDict)
+            elif e_code in [17,16]:
+                sendDevDict(devDict)
+                if e_code == 17 :
+                    devDict['crossUpDown']=0
+                if e_code == 16 :
+                    devDict['crossLR']=0
+                sendDevDict(devDict)
+            else:
+                if mutePrint: print "e_code :",e_code
+                if mutePrint: print "e_type :",e_code
+                if mutePrint: print "e_value :",e_value
+                sendDevDict(devDict)
