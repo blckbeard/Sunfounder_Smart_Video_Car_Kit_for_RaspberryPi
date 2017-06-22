5bf3b5fcb431e06df36a966c43937b81df8ba40e
blackbeard <blackbeard>
2017-06-22 17:19:24 +0200

    joypad

--- joypad/JoypadCar_Server.py
+++ joypad/JoypadCar_Server.py
@@ -64,7 +64,11 @@ while True:
 		print data
 		print type(data)
 		cleanData = data.replace("][", "],[")
-		devDict = eval(cleanData[0])
+		cleanData = eval(cleanData)
+		if type(cleanData) == str:
+			devDict = cleanData
+		elif type(cleanData)== list:
+			devDict = cleanData[0]
 		print devDict
 		'''
 		if not data:
